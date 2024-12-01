6437958285f28b721133e4a394667d4eddd0c6d2
Blum Balázs Gábor <61165242+blumbalazs@users.noreply.github.com>
2024-12-01 14:20:20 +0100

    implement day 1.2 challange solution

--- day1/challange2.go
+++ day1/challange2.go
@@ -1,5 +1,23 @@
 package main
 
 func CalculateSimilarityScore(leftSideList []int, rightSideList []int) int {
-	return 0
+
+	leftSideOccurance := countValueOccurance(leftSideList)
+
+	similarityScore := 0
+	for _, value := range rightSideList {
+		similarityScore += value * leftSideOccurance[value]
+	}
+
+	return similarityScore
+}
+
+func countValueOccurance(list []int) map[int]int {
+	occurance := map[int]int{}
+
+	for _, value := range list {
+		occurance[value] = occurance[value] + 1
+	}
+
+	return occurance
 }
