ac6bda06882a901eb6105e74a5f8c77d67ada0ae
Blum Balázs Gábor <61165242+blumbalazs@users.noreply.github.com>
2024-12-02 22:24:50 +0100

    create day2 template

---
+++ day2/day2_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+var example = [][]int{
+	{7, 6, 4, 2, 1},
+	{1, 2, 7, 8, 9},
+	{9, 7, 6, 2, 1},
+	{1, 3, 2, 4, 5},
+	{8, 6, 4, 4, 1},
+	{1, 3, 6, 7, 9},
+}
+
+func TestChallange1Example(t *testing.T) {
+
+}
+func TestChallange2Example(t *testing.T) {
+
+}
