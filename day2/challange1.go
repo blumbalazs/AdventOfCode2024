6ccccba3c7c6993323a86adeda0786054654ee77
Blum Balázs Gábor <61165242+blumbalazs@users.noreply.github.com>
2024-12-02 23:36:03 +0100

    added solution to challange 2.1

--- day2/challange1.go
+++ day2/challange1.go
@@ -1,5 +1,25 @@
 package main
 
+import "math"
+
 func CalculateNumberOfSafeReports(reportsOfLevels [][]int) int {
-	return 0
+
+	numberOfSafeReports := 0
+	for _, levels := range reportsOfLevels {
+
+		growing := levels[0] < levels[1]
+		stillGood := true
+		for i := 1; i < len(levels) && stillGood; i++ {
+			isDirectionGood := (levels[i] > levels[i-1]) == growing
+			delta := int(math.Abs(float64(levels[i] - levels[i-1])))
+			if !(isDirectionGood && delta >= 1 && delta <= 3) {
+				stillGood = false
+			}
+		}
+		if stillGood {
+			numberOfSafeReports++
+		}
+	}
+
+	return numberOfSafeReports
 }
